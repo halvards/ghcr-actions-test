14cc2f5eed6d93a1363894f23aedd635c6613de2
Halvard Skogsrud <halvard@google.com>
2022-01-07 13:13:47 +1100

    Add http server to sample app

--- main.go
+++ main.go
@@ -2,10 +2,24 @@ package main
 
 import (
 	"fmt"
+	"net/http"
+	"os"
 
 	"github.com/halvards/ghcr-actions-test/pkg/version"
 )
 
 func main() {
-	fmt.Println(version.Version)
+	if len(os.Args) > 1 && os.Args[1] == "version" {
+		fmt.Println(version.Version)
+		return
+	}
+	port := os.Getenv("PORT")
+	if port == "" {
+		port = "8080"
+	}
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, version.Version)
+	})
+	fmt.Printf("Listening on port %v\n", port)
+	http.ListenAndServe(":"+port, nil)
 }
